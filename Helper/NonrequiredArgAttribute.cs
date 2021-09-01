29bbc4693af0c5afb88eeac2ef4ab5712857f384
RandomiaGaming <randomiagaming@gmail.com>
2021-09-01 12:17:32 -0700

    Updated Icon for higher resolution

---
+++ Helper/NonrequiredArgAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Helper
+{
+    [AttributeUsage(AttributeTargets.Parameter)]
+    public sealed class OptionalArgumentAttribute : Attribute
+    {
+        public readonly bool optional = false;
+        public OptionalArgumentAttribute(bool optional = false)
+        {
+            this.optional = optional;
+        }
+    }
+}
